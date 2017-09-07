690b2a6579704b4d1bb34500e614bd00b124dc72
kendall <motesmass@gmail.com>
2017-09-06 21:12:29 -0600

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.kValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(308, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Solve";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(69, 23);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(177, 26);
+            this.Input.TabIndex = 3;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(69, 93);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(198, 26);
+            this.Output.TabIndex = 4;
+            // 
+            // kValue
+            // 
+            this.kValue.Location = new System.Drawing.Point(69, 161);
+            this.kValue.Name = "kValue";
+            this.kValue.Size = new System.Drawing.Size(100, 26);
+            this.kValue.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 406);
+            this.Controls.Add(this.kValue);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.TextBox kValue;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //takes the input form the input box.
+            int inputValue = Convert.ToInt32(Input.Text);
+            // convertes input from string to long
+            long possible_prime = (Convert.ToInt64(Input.Text));
+            //checks if number generated is prime
+            if (is_It_Prime(possible_prime))
+            {
+                //uses the given algorith to find the probability and then prints it
+                double k = Convert.ToDouble(kValue.Text);
+                Output.Text = "prime, Probablity: " + 100.0 * (1.0 / (Math.Pow(2.0, k))) + "%";
+            }
+            else
+            {
+                Output.Text = "Not Prime.";
+            }
+        }
+
+        // this funciton checks to see if the number n, random generated integers are prime
+        //this is O(n^3)
+        //? or is it O(n) book is misslead ing page 29
+        private bool is_It_Prime(long n)
+        {
+
+            Random rnd = new Random();
+            //converts 
+            int k = (Convert.ToInt32(kValue.Text));
+            //creates an array list the size of the given k value
+            int[] rands = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                // adds randomly generated umbers to the array of numbers between the n values. 
+                rands[i] = rnd.Next(2, (int)n - 1);
+            }
+
+            //iterates over the random number list
+            foreach (long r in rands)
+            {
+                //runs the given moduler exponentiation algorithm on each r number
+                long exp = modular_exponentiation((int)r, ((int)n - 1), (int)n);
+                //check to see if it prime or not and returns the desired response
+                if (exp == 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //this function recursivly finds xmod N. It keeps the bit size down of the calculations by dividing repeatedly multiplying by x modulo N
+        //this is O(n^3)
+        private long modular_exponentiation(long x, long y, long n)
+        {
+            //check if we met our base case of y = 0
+            if (y == 0)
+                return 1;
+            long z = modular_exponentiation(x, y / 2, n);
+            //check if y is an odd number
+            if ((y & 1) == 1)
+            {
+                // return this value for the recursion process to run on the returned value
+                long returnValue = x * ((z * z) % n) % n;
+                return returnValue;
+            }
+            else
+            {
+                // return this value for the recursion process to run on the returned value
+                long returnValue = (z * z) % n;
+                return returnValue;
+            }
+        }
+    }
+}
